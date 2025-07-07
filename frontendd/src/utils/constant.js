038789bf922c10a95d13ca6df9cdd0c468a64664
Rushit Asodariya <rkpatel74360@gmail.com>
2025-07-07 17:31:44 +0530

    Update constant.js

--- frontendd/src/utils/constant.js
+++ frontendd/src/utils/constant.js
@@ -2,3 +2,4 @@ export const USER_API_END_POINT="https://job-portal-2-ific.onrender.com/api/v1/u
 export const JOB_API_END_POINT="https://job-portal-2-ific.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-portal-2-ific.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://job-portal-2-ific.onrender.com/api/v1/company";
+const API_BASE_URL = "https://job-portal-2-ific.onrender.com";
